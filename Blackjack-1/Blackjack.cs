3d44986cdc408b2c50a06021a9f32db94688d366
JUYOUNGCHAN <everfaul@gmail.com>
2026-03-01 21:52:59 +0900

    blackjack임시2

---
+++ Blackjack-1/Blackjack.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Blackjack player = new Blackjack("플레이어");
+        Blackjack dealer = new Blackjack("딜러");
+
+        bool gameContinue = true;
+
+        while (gameContinue)
+        {
+            Blackjack.ResetGame();
+            player.ResetHand();
+            dealer.ResetHand();
+            Console.Clear();
+            Console.WriteLine("=== 새 게임 시작 === ");
+            Console.WriteLine("\n카드를 섞는 중...");
+
+            Console.WriteLine("\n=== 초기패 ===");
+
+            player.Drawcard();
+            player.Drawcard();
+            dealer.Drawcard();
+            dealer.Drawcard();
+
+            while (player.TotalScore < 21)
+            {
+                dealer.ShowAllCards(isFirstRound: true);
+                player.ShowAllCards();
+                player.ShowScore();
+
+                Console.Write("\n H(Hit) 또는 S(Stand)를 선택하세요: ");
+                string choice = Console.ReadLine().ToUpper();
+
+                if (choice == "H")
+                {
+                    player.Drawcard();
+                    player.ShowLastCard();
+                }
+                else if (choice == "S")
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("\n============================");
+            Console.WriteLine("=== 게임 결과 ===");
+            dealer.ShowAllCards();
+            dealer.ShowScore();
+            Console.WriteLine();
+            player.ShowAllCards();
+            player.ShowScore();
+            Console.WriteLine("============================");
+
+            Winner(player.TotalScore, dealer.TotalScore);
+
+            Console.Write("\n새 게임을 하시겠습니까? (Y/N): ");
+            gameContinue = Console.ReadLine().ToUpper() == "Y";
+        }
+    }
+
+    static void Winner(int pScore, int dScore)
+    {
+        if (pScore > 21)
+        {
+            Console.WriteLine("버스트! 21을 초과했습니다. 딜러 승리!");
+        }
+        else if (dScore > 21)
+        {
+            Console.WriteLine("버스트! 21을 초과했습니다. 플레이어 승리!");
+        }
+        else if (pScore > dScore)
+        {
+            Console.WriteLine("플레이어 승리!");
+        }
+        else if (pScore < dScore)
+        {
+            Console.WriteLine("딜러 승리!");
+        }
+        else
+        {
+            Console.WriteLine("무승부(Push)입니다.");
+        }
+    }
+}
+
--- Blackjack-1/Program.cs
+++ Blackjack-1/Program.cs
@@ -1,86 +1,154 @@
 using System;
-using System.Text;
-
+using System.Net.Sockets;
 class Blackjack
 {
-    static int[,] card = new int[4, 13];
-    static bool[,] usedcard = new bool[4, 13];
-    static string[] playercard;
-    static string[] dealercard;
+    private static string[,] cardDeck = new string[4, 13];
+    private static bool[,] isUsed = new bool[4, 13];
+    private static bool isDealerStop = false;
+    private string name;
+    private int cardCount = 0;
+    private int[] cardshape = new int[11];
+    private int[] cardnumber = new int[11];
 
-    public Blackjack()
+    public int TotalScore
     {
+        get
+        {
+            int score = 0;
+            int Acount = 0;
+
+            for (int i = 0; i < cardCount; i++)
+            {
+                int valueIdx = cardnumber[i];
+
+                if (valueIdx == 0)
+                {
+                    Acount++;
+                    score += 11;
+                }
+                else if (valueIdx >= 9)
+                {
+                    score += 10;
+                }
+                else
+                {
+                    score += (valueIdx + 1);
+                }
+            }
+
+            while (score > 21 && Acount > 0)
+            {
+                score -= 10;
+                Acount--;
+            }
+
+            if (name == "딜러" && score >= 17)
+            {
+                isDealerStop = true;
+            }
+            return score;
+
+        }
+    }
+
+    public Blackjack(string name)
+    {
+        this.name = name;
+        string[] shapes = { "♠", "◆", "♥", "♣" };
+        string[] numbers = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 
         for (int i = 0; i < 4; i++)
         {
             for (int j = 0; j < 13; j++)
-                card[i, j] = j + 1;
-
-        }
-    }
-
-
-
-    static public void Select()
-    {
-        int shape = 0;
-        int number = 0;
-        while (true)
-        {
-            Random random = new Random();
-
-            shape = random.Next(1, 5);
-            number = random.Next(1, 14);
-
-
-            if (usedcard[shape, number] == false)
-                break;
-
-        }
-        usedcard[shape, number] = true;
-
-
-    }
-    public string Printcard(int shape, int number)
-    {
-        StringBuilder sb = new StringBuilder();
-
-        switch (shape)
-        {
-            case 1:
-                sb.Append("♠");
-                break;
-            case 2:
-                sb.Append("♥");
-                break;
-            case 3:
-                sb.Append("♣");
-                break;
-            case 4:
-                sb.Append("♦");
-                break;
-        }
-        if (number > 1 && number < 11) { return sb.Append(number).ToString(); }
-        else
-        {
-            switch (number)
             {
-                case 1:
-                    return sb.Append("A").ToString();
-
-                case 11:
-                    return sb.Append("J").ToString();
-
-                case 12:
-                    return sb.Append('Q').ToString();
-
-                case 13:
-                    return sb.Append('K').ToString();
-                default:
-                    return "";
-
+                cardDeck[i, j] = shapes[i] + numbers[j];
             }
         }
     }
 
+    public void Drawcard()
+    {
+        Random random = new Random();
+        while (true)
+        {
+            int shape = random.Next(0, 4);
+            int number = random.Next(0, 13);
+
+            if (!isUsed[shape, number])
+            {
+                isUsed[shape, number] = true;
+                cardshape[cardCount] = shape;
+                cardnumber[cardCount] = number;
+                cardCount++;
+                break;
+            }
+        }
+    }
+
+    public void ShowLastCard()
+    {
+        int last = cardCount - 1;
+        Console.WriteLine($"{name}가 카드를 받았습니다: {GetCardName(cardshape[last], cardnumber[last])}");
+    }
+
+    public void ShowAllCards(bool isFirstRound = false)
+    {
+        Console.Write($"{name}의 패: ");
+
+        if (name == "딜러" && isFirstRound)
+        {
+            Console.WriteLine("[??], " + GetCardName(cardshape[1], cardnumber[1]));
+            Console.WriteLine("딜러의 점수: ?");
+        }
+        else
+        {
+            for (int i = 0; i < cardCount; i++)
+            {
+                Console.Write(GetCardName(cardshape[i], cardnumber[i]) + (i == cardCount - 1 ? "" : ", "));
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public void ShowScore(bool isFirstRound = false)
+    {
+        if (name == "딜러" && isFirstRound)
+        {
+            Console.WriteLine($"{name}의 현재 점수: ?");
+        }
+        else
+        {
+            int score = TotalScore;
+            Console.Write($"{name} 점수: {score}");
+            if (score == 21) Console.Write(" (블랙잭!)");
+            Console.WriteLine();
+        }
+    }
+
+
+
+
+public static void ResetGame()
+    {
+        Array.Clear(isUsed, 0, isUsed.Length);
+        isDealerStop = false;
+    }
+
+    public static string GetCardName(int shapeIdx, int valueIdx)
+    {
+        return cardDeck[shapeIdx, valueIdx];
+    }
+
+    public void Resethand()
+    {
+
+    }
+    public void ResetHand()
+    {
+        cardCount = 0;
+        Array.Clear(cardshape, 0, cardshape.Length);
+        Array.Clear(cardnumber, 0, cardnumber.Length);
+    }
+
 }
+
